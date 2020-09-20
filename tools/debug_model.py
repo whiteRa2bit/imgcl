e5be5a927fc773d34af5cf1b19d5ab7637e5312b
whiteRa2bit <pavel.fakanov@gmail.com>
2020-09-20 12:37:42 +0000

    Fix debug script

--- tools/debug_model.py
+++ tools/debug_model.py
@@ -1,9 +1,10 @@
 from imgcl.nets.alexnet import Model
 from imgcl.nets.utils import debug_model
+from imgcl.config import TRAIN_CONFIG
 
 
 def main():
-    model = Model({"dropout": 0.5})
+    model = Model(TRAIN_CONFIG)
     debug_model(model)
 
 
